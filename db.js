0843fd4da02192c00f5fab6aef000017c100147e
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2020-08-30 17:19:20 +0530

    Script For Syncing Database

---
+++ db.js
@@ -0,0 +1,69 @@
+const Sequelize = require('sequelize')
+
+const db = new Sequelize('ttbs_db', 'ttbs', 'ttbs_password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    pool: {
+        min: 0,
+        max: 5
+    }
+})
+
+const Show_Details = db.define('show_details', {
+    date: {
+        type: Sequelize.DATEONLY,
+        primaryKey: true
+    },
+    time: {
+        type: Sequelize.TINYINT.UNSIGNED,
+        primaryKey: true
+    },
+    ticket_count: {
+        type: Sequelize.TINYINT.UNSIGNED,
+        allowNull: false
+    }
+})
+
+const Registered_Users = db.define('registered_users', {
+    user_id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    phone: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        allowNull: false
+    }
+})
+
+const Tickets_Sold = db.define('tickets_sold', {
+    ticket_id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+    },
+    time: {
+        type: Sequelize.TINYINT.UNSIGNED,
+        allowNull: false
+    },
+    user_id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        allowNull: false
+    }
+})
+
+db.sync()
+    .then(() => console.log("Database Has Been Synced"))
+    .catch((err) => console.error("Error Creating Database"))
+
+exports = module.exports = {
+    Show_Details, Registered_Users, Tickets_Sold
+}
