94f0f32e318a1ebb739de632f36679f94b35a75e
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2020-08-30 23:34:36 +0530

    NodeJS Server With Auto-Delete For Old Records

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const server = express()
+const schedule = require('node-schedule')
+
+const Show_Details = require('./db').Show_Details
+const Tickets_Sold = require('./db').Tickets_Sold
 
 server.use(express.json())
 server.use(express.urlencoded({extended: true}))
@@ -14,6 +18,35 @@ server.use('/delete_ticket', require('./routes/delete_ticket').route)
 server.use('/view_user', require('./routes/view_user').route)
 server.use('/view_tickets', require('./routes/view_tickets').route)
 server.use('/all_shows', require('./routes/all_shows').route)
+
+// Auto-Delete Old Tickets (8 Hours Old)
+schedule.scheduleJob('0 0 * * * *', () => {
+    var hr = new Date;
+    var hour = hr.getHours();
+
+    var today = new Date().toJSON().slice(0, 10)
+    var prevDate = new Date(today)
+    prevDate.setDate(prevDate.getDate() - 1)
+    prevDate = prevDate.toJSON().slice(0, 10)
+
+    if(hour == 20) { // For 12:00 Show
+        // Delete Shows Where Time == 12 On Same Date
+        Tickets_Sold.destroy({ where: { date: today, time: 12 } })
+        Show_Details.destroy({ where: { date: today, time: 12 } })
+    } else if (hour == 23) { // For 15:00 Show
+        // Delete Shows Where Time == 15 On Same Date
+        Tickets_Sold.destroy({ where: { date: today, time: 15 } })
+        Show_Details.destroy({ where: { date: today, time: 15 } })
+    } else if (hour == 2) { // For 18:00 Show
+        // Delete Shows Where Time == 18 On Previous Date
+        Tickets_Sold.destroy({ where: { date: prevDate, time: 18 } })
+        Show_Details.destroy({ where: { date: prevDate, time: 18 } })
+    } else if (hour == 5) { // For 21:00 Show
+        // Delete Shows Where Time == 21 On Previous Date
+        Tickets_Sold.destroy({ where: { date: prevDate, time: 21 } })
+        Show_Details.destroy({ where: { date: prevDate, time: 21 } })
+    } 
+})
 
 // Invalid API Request
 server.use((req, res) => res.send("404 Invalid API Call"))
