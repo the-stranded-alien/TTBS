e0079da9d67d30cc0177b4c16d3eb92021a2632e
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2020-08-31 01:29:13 +0530

    API Route To Book A Ticket (Endpoint - 1)

--- routes/book_ticket.js
+++ routes/book_ticket.js
@@ -3,19 +3,6 @@ const Registered_Users = require('../db').Registered_Users
 const Show_Details = require('../db').Show_Details
 const Tickets_Sold = require('../db').Tickets_Sold
 
-// Send Details Of All Booked Tickets (Only For Testing Purpose)
-route.get('/', (req, res) => {
-    Tickets_Sold.findAll()
-        .then((tickets) => {
-            res.status(200).send(tickets)
-        })
-        .catch((err) => {
-            res.status(500).send({
-                error: "Couldn't Retrieve Booked Ticket Details"
-            })
-        })
-})
-
 // Booking Ticket ->
 // Conditions Checked Before Booking A Ticket:
 // 1. Check Whether The User Is Registered, If Yes Fetch His User-Id And Book Ticket
@@ -24,74 +11,87 @@ route.get('/', (req, res) => {
 // Show With Count As 1. Also If Count == 20 Return Error Message That Show Is Already Full.
 // If Everything Is Fine Add The Ticket To Booked Tickets With Date, Time, And User_Id. 
 route.post('/', (req, res) => {
-    Registered_Users.findOne({
-        where: {name: req.body.name, phone: req.body.phone}
-    })
-    .then((user) => {
-        if(user !== null) {
-            Show_Details.findOrCreate({
-                where: {date: req.body.date, time: req.body.time},
-                defaults: {
-                    ticket_count: 1
-                }
+    const inpHour = req.body.time
+    const currDate = new Date().getTime();
+    const currHour = new Date().getHours();
+    const inpDate = new Date(req.body.date).getTime();
+    if(inpDate >= currDate) {
+        if((inpHour >= currHour) && ((inpHour == 12) || (inpHour == 15) || (inpHour == 18) || (inpHour == 21))) {
+            Registered_Users.findOne({
+                where: {name: req.body.name, phone: req.body.phone}
             })
-            .then(([show, created]) => {
-                if(created === true) {
-                    // First Ticket Booking For This Show, Can Book A Ticket.
-                    Tickets_Sold.create({
-                        date: req.body.date,
-                        time: req.body.time,
-                        user_id: user.user_id
+            .then((user) => {
+                if(user !== null) {
+                    Show_Details.findOrCreate({
+                        where: {date: req.body.date, time: req.body.time},
+                        defaults: {
+                            ticket_count: 1
+                        }
                     })
-                    .then((booking) => {
-                        res.status(201).send(booking)
+                    .then(([show, created]) => {
+                        if(created === true) {
+                            // First Ticket Booking For This Show, Can Book A Ticket.
+                            Tickets_Sold.create({
+                                date: req.body.date,
+                                time: req.body.time,
+                                user_id: user.user_id
+                            })
+                            .then((booking) => {
+                                res.status(201).send(booking)
+                            })
+                            .catch((err) => {
+                                res.status(501).send("Couldn't Book Tickets.")
+                            })
+                        }
+                        else {
+                            // This Show Already Exists, So
+                            // First We Need To Check The Count Of Tickets, And 
+                            // We Can Book A Ticket Only If Count < 20.
+                            if(show.ticket_count < 20) {
+                                // After Booking The Ticket Also Increase The Count In Show Details
+                                Tickets_Sold.create({
+                                    date: req.body.date,
+                                    time: req.body.time,
+                                    user_id: user.user_id
+                                })
+                                .then((booking) => {
+                                    Show_Details.update({ ticket_count: (show.ticket_count + 1)}, {
+                                        where: {
+                                            date: req.body.date,
+                                            time: req.body.time      
+                                        }
+                                    })
+                                    res.status(201).send(booking)
+                                })
+                                .catch((err) => {
+                                    res.status(501).send("Couldn't Book Tickets.")
+                                })
+        
+                            } else {
+                                res.status(202).send("Show Already Booked Up ! Choose Some Other Timing.")
+                            }
+                        }
                     })
                     .catch((err) => {
-                        res.status(501).send("Couldn't Book Tickets.")
+                        res.status(501).send({
+                            error: "Couldn't Check For Show Details."
+                        })
                     })
                 }
-                else {
-                    // This Show Already Exists, So
-                    // First We Need To Check The Count Of Tickets, And 
-                    // We Can Book A Ticket Only If Count < 20.
-                    if(show.ticket_count < 20) {
-                        // After Booking The Ticket Also Increase The Count In Show Details
-                        Tickets_Sold.create({
-                            date: req.body.date,
-                            time: req.body.time,
-                            user_id: user.user_id
-                        })
-                        .then((booking) => {
-                            Show_Details.update({ ticket_count: (show.ticket_count + 1)}, {
-                                where: {
-                                    date: req.body.date,
-                                    time: req.body.time      
-                                }
-                            })
-                            res.status(201).send(booking)
-                        })
-                        .catch((err) => {
-                            res.status(501).send("Couldn't Book Tickets.")
-                        })
-
-                    } else {
-                        res.status(202).send("Show Already Booked Up ! Choose Some Other Timing.")
-                    }
-                }
+                else res.status(401).send("Unregistered User. Please Register Yourself First.")
             })
             .catch((err) => {
                 res.status(501).send({
-                    error: "Couldn't Check For Show Details."
+                    error: "Couldn't Check For Registered Users."
                 })
             })
         }
-        else res.status(401).send("Unregistered User. Please Register Yourself First.")
-    })
-    .catch((err) => {
-        res.status(501).send({
-            error: "Couldn't Check For Registered Users."
-        })
-    })
+        else {
+            res.status(501).send("Enter Valid Show Time & Try Again")
+        }
+    } else {
+        res.status(501).send("Enter Valid Date & Time For The Show & Try Again")
+    }
 })
 
 exports = module.exports = {
