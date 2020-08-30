c51477f965ef080ae5e7ac9982e113ea4350e639
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2020-08-31 01:28:37 +0530

    API Route To Register A New User

--- routes/new_user.js
+++ routes/new_user.js
@@ -1,19 +1,6 @@
 const Registered_Users = require('../db').Registered_Users
 const route = require('express').Router()
 
-// Send Details Of All Registered User (Only For Testing Purpose)
-route.get('/', (req, res) => {
-    Registered_Users.findAll()
-        .then((users) => {
-            res.status(200).send(users)
-        })
-        .catch((err) => {
-            res.status(500).send({
-                error: "Couldn't Retrieve Users."
-            })
-        })
-})
-
 // Adding A New User To Our Database, Only If It Doesn't Exists Already.
 route.post('/', (req, res) => {
     Registered_Users.findOrCreate({
