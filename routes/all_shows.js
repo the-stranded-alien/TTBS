e15b7dd6d2e9ca09225c25c7fde135ba61a44816
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2020-08-31 01:27:01 +0530

    API Route To Fetch All Shows Details (Only For Testing)

--- routes/all_shows.js
+++ routes/all_shows.js
@@ -2,6 +2,7 @@ const { Op } = require('sequelize')
 const route = require('express').Router()
 const Show_Details = require('../db').Show_Details
 
+// Send Details Of All Shows (Only For Testing Purpose)
 route.get('/', (req, res) => {
     Show_Details.findAll({ where: {ticket_count: {[Op.ne]: 0}} })
         .then((shows) => {
